f8810a4cb3a0b1e140b683f2f8c7f24d7779472c
Guilherme Capitão <guilherme.capitao95@gmail.com>
2021-02-23 01:25:42 -0300

    aula01

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ExperienceBar } from './components/ExperienceBar';
+
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
